c26be7df96d725bc01099153982e4c2ea70aab31
pathetic <74772768+pathetic@users.noreply.github.com>
2025-05-03 20:18:05 +0300

    process viewer now has suspend/resume + start process

--- client/src/features/process.rs
+++ client/src/features/process.rs
@@ -1,5 +1,12 @@
 use sysinfo::{ System, Pid };
 use common::packets::{ ProcessList, Process };
+use std::process::Command;
+
+use winapi::shared::minwindef::FALSE;
+use winapi::um::handleapi::CloseHandle;
+use winapi::um::processthreadsapi::{OpenThread, ResumeThread, SuspendThread};
+use winapi::um::tlhelp32::{CreateToolhelp32Snapshot, TH32CS_SNAPTHREAD, Thread32First, Thread32Next, THREADENTRY32};
+use winapi::um::winnt::{HANDLE, THREAD_SUSPEND_RESUME};
 
 pub fn process_list() -> ProcessList {
     let mut s = System::new_all();
@@ -29,5 +36,63 @@ pub fn kill_process(pid: usize) {
 
     if let Some(process) = s.process(Pid::from(pid)) {
         process.kill();
+    }
+}
+
+pub fn start_process(name: String) {
+    Command::new(name).spawn().unwrap();
+}
+
+pub fn suspend_process(pid: usize) {
+    let pid = pid as u32;
+    unsafe {
+        let te: &mut THREADENTRY32 = &mut std::mem::zeroed();
+        (*te).dwSize = std::mem::size_of::<THREADENTRY32>() as u32;
+
+        let snapshot: HANDLE = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+
+        if Thread32First(snapshot, te) == 1 {
+            loop {
+                if pid == (*te).th32OwnerProcessID {
+                    let tid = (*te).th32ThreadID;
+
+                    let thread: HANDLE = OpenThread(THREAD_SUSPEND_RESUME, FALSE, tid);
+                    SuspendThread(thread) as i32 == -1i32;
+                    CloseHandle(thread);
+                }
+
+                if Thread32Next(snapshot, te) == 0 {
+                    break;
+                }
+            }
+        }
+        CloseHandle(snapshot);
+    }
+}
+
+pub fn resume_process(pid: usize) {
+    let pid = pid as u32;
+    unsafe {
+        let te: &mut THREADENTRY32 = &mut std::mem::zeroed();
+        (*te).dwSize = std::mem::size_of::<THREADENTRY32>() as u32;
+
+        let snapshot: HANDLE = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+
+        if Thread32First(snapshot, te) == 1 {
+            loop {
+                if pid == (*te).th32OwnerProcessID {
+                    let tid = (*te).th32ThreadID;
+
+                    let thread: HANDLE = OpenThread(THREAD_SUSPEND_RESUME, FALSE, tid);
+                    ResumeThread(thread) as i32 == -1i32;
+                    CloseHandle(thread);
+                }
+
+                if Thread32Next(snapshot, te) == 0 {
+                    break;
+                }
+            }
+        }
+        CloseHandle(snapshot);
     }
 }
